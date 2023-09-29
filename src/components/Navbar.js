abf1afa64574af7fac3c55a835f7979bfb6a19b4
siwanijaiswal <siwani.jce20@sot.pdpu.ac.in>
2023-09-29 19:20:13 +0530

    changed navbar brand color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,9 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+      
+        <li className="navbar-brand" >{props.title}</li>
+        {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
